654abe0823c02257a922144c3c3d891222c60f27
jaddu17 <shafajaddu@gmail.com>
2026-01-02 18:11:15 +0700

    membuat interface StatusUiSiswa

---
+++ app/src/main/java/com/example/pertemuan14/viewmodel/HomeViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.pertemuan14.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan14.modeldata.Siswa
+import com.example.pertemuan14.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}

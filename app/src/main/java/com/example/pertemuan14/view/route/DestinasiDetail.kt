f24fcb3b20814dcf343a1ee4ad9965c3f27c0299
jaddu17 <shafajaddu@gmail.com>
2026-01-02 18:16:22 +0700

    membuat destinasidetail

---
+++ app/src/main/java/com/example/pertemuan14/view/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan14.view.route
+
+import com.example.pertemuan14.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}

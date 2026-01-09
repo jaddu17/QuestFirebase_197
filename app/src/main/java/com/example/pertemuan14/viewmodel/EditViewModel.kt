3954c02f301a9a7748650fef47c11a18b463bfdf
jaddu17 <shafajaddu@gmail.com>
2026-01-09 15:40:05 +0700

    memgambil data siswa dari id, terus di kirim ke uistate

---
+++ app/src/main/java/com/example/pertemuan14/viewmodel/EditViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.pertemuan14.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan14.modeldata.DetailSiswa
+import com.example.pertemuan14.modeldata.UIStateSiswa
+import com.example.pertemuan14.modeldata.toDataSiswa
+import com.example.pertemuan14.modeldata.toUiStateSiswa
+import com.example.pertemuan14.repositori.RepositorySiswa
+import com.example.pertemuan14.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+
+
+}

459dc81a0fb21be20ae1fef7fefe16c85fd23d0d
jaddu17 <shafajaddu@gmail.com>
2026-01-09 15:51:43 +0700

    menambahkan petanavigasi

--- app/src/main/java/com/example/pertemuan14/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan14/view/controllNavigasi/PetaNavigasi.kt
@@ -1,14 +1,20 @@
 package com.example.pertemuan14.view.controllNavigasi
 
+import android.R.attr.type
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pertemuan14.view.DetailSiswaScreen
+import com.example.pertemuan14.view.EditSiswaScreen
 import com.example.pertemuan14.view.EntrySiswaScreen
 import com.example.pertemuan14.view.HomeScreen
 import com.example.pertemuan14.view.route.DestinasiDetail
+import com.example.pertemuan14.view.route.DestinasiEdit
 import com.example.pertemuan14.view.route.DestinasiEntry
 import com.example.pertemuan14.view.route.DestinasiHome
 
@@ -34,5 +40,19 @@ fun HostNavigasi(
                 navController.navigate(DestinasiHome.route)
             })
         }
+        composable(DestinasiDetail.routeWithArgs, arguments = listOf(navArgument
+            (DestinasiDetail.itemIdArg) {
+            type = NavType. StringType})
+        ){
+            DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit
+                .route}/$it")},
+                navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument
+            (DestinasiEdit.itemIdArg){
+            type= NavType.StringType})){
+            EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() })
+            }
     }
 }
